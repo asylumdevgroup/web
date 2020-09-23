322f53a9a4b70e22be2903d8f3d622dc9ff822e7
jarodsstuff <legodata@comcast.net>
2020-09-22 20:43:33 -0400

    Updates

---
+++ precache-manifest.7788e347aedc0187b2de857813a4d3d8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2805e0f0ccfe7979faf13f9c1cc2d0b9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8e65aa08c21361127731",
+    "url": "/static/css/2.8aa5a7f8.chunk.css"
+  },
+  {
+    "revision": "77bdcd84d34db90a5efa",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "8e65aa08c21361127731",
+    "url": "/static/js/2.143a6003.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.143a6003.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "77bdcd84d34db90a5efa",
+    "url": "/static/js/main.f4785b3a.chunk.js"
+  },
+  {
+    "revision": "dc886f43312526a4edbd",
+    "url": "/static/js/runtime-main.f81998f3.js"
+  }
+]);
